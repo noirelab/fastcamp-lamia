8008ed5bd249bba92c4159e023f80250ce0162fd
noirelab <kaiquemedeiroslima05@gmail.com>
2026-09-21 16:53:49 -0300

    fix(setup): corrige lang do html para pt-BR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
     <html
-      lang="en"
+      lang="pt-BR"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">{children}</body>
